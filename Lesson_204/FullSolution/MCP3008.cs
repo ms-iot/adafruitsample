d02e40a7d49faf6693bf0be1ad01d3aa107a8f9e
David Shoemaker <David.Shoemaker@microsoft.com>
2015-09-14 17:07:18 -0700

    Lesson 204 first commit Full Solution (first commit) Skeleton solution (copy of full) Content (empty) Fritzing diagram (first commit)

---
+++ Lesson_204/FullSolution/MCP3008.cs
@@ -0,0 +1,116 @@
+﻿using Windows.Devices.Enumeration;
+using Windows.Devices.Spi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lesson_204
+{
+    class MCP3008
+    {
+        private SpiDevice mcp3008;
+        const int SPI_CHIP_SELECT_LINE = 0;  // SPI0 CS0 pin 24
+        const byte MCP3008_SingleEnded = 0x08;
+        const byte MCP3008_Differential = 0x00;
+
+        float ReferenceVolgate;
+
+        public const uint Min = 0;
+        public const uint Max = 1023;
+
+        public MCP3008(float referenceVolgate)
+        {
+            ReferenceVolgate = referenceVolgate;
+        }
+
+        public async void Initialize()
+        {
+            try
+            {
+                var settings = new SpiConnectionSettings(SPI_CHIP_SELECT_LINE);
+                settings.ClockFrequency = 3600000;                              // 3.6MHz is the rated speed of the MCP3008 at 5v
+                settings.Mode = SpiMode.Mode0;
+
+
+
+                string aqs = SpiDevice.GetDeviceSelector();                     /* Get a selector string that will return all SPI controllers on the system */
+                var dis = await DeviceInformation.FindAllAsync(aqs);            /* Find the SPI bus controller devices with our selector string             */
+                mcp3008 = await SpiDevice.FromIdAsync(dis[0].Id, settings);    /* Create an SpiDevice with our bus controller and SPI settings             */
+                if (mcp3008 == null)
+                {
+                    Debug.WriteLine(
+                        "SPI Controller {0} is currently in use by " +
+                        "another application. Please ensure that no other applications are using SPI.",
+                        dis[0].Id);
+                    return;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception: " + e.Message + "\n" + e.StackTrace);
+                throw;
+            }
+        }
+
+
+        bool Init = false;
+        private async Task begin()
+        {
+            Debug.WriteLine("MCP3008::begin");
+
+            // No startup requirements for this part.
+            await Task.Delay(5);
+
+            Init = true;
+        }
+
+        public async Task<int> ReadADC(byte whichChannel)
+        {
+            // To line everything up for ease of reading back (on byte boundary) we 
+            // will pad the command start bit with 7 leading "0" bits
+
+            // Write 0000 000S GDDD xxxx xxxx xxxx
+            // Read  ???? ???? ???? ?N98 7654 3210
+            // S = start bit
+            // G = Single / Differential
+            // D = Chanel data 
+            // ? = undefined, ignore
+            // N = 0 "Null bit"
+            // 9-0 = 10 data bits
+
+            if (!Init) await begin();
+
+            byte command = whichChannel;
+            command |= MCP3008_SingleEnded;
+            command <<= 4;
+
+            byte[] commandBuf = new byte[] { 0x01, command, 0x00 };
+
+            byte[] readBuf = new byte[] { 0x00, 0x00, 0x00 };
+
+            mcp3008.TransferFullDuplex(commandBuf, readBuf);
+
+            int sample = readBuf[2] + ((readBuf[1] & 0x03) << 8);
+            int s2 = sample & 0x3FF;
+            Debug.Assert(sample == s2);
+
+            //Debug.WriteLine("MCP3008::ReadADC C:{0} {1}", whichChannel, sample.ToString());
+
+            return sample;
+        }
+
+        /// <summary>
+        ///  Returns the ADC value (uint) as a float voltage based on the configured reference voltage
+        /// </summary>
+        /// <param name="adc"> the ADC value to convert</param>
+        /// <returns>The computed voltage based on the reference voltage</returns>
+        public float ADCToVoltage(int adc)
+        {
+            return (float)adc * ReferenceVolgate / (float)Max;
+        }
+    }
+}
---
+++ Lesson_204/FullSolution/MainPage.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lesson_204
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const float ReferenceVoltage = 3.3F;
+        const byte LowPotentiometerADCChannel = 0;
+        const byte HighPotentiometerADCChannel = 1;
+        const byte CDSADCChannel = 2;
+        const string JustRightLightString = "Ah, just right";
+        const string LowLightString = "I need a light";
+        const string HighLightString = "I need to wear shades";
+
+        enum eState { unknown, JustRight, TooBright, TooDark};
+        eState CurrentState = eState.unknown;
+
+        MCP3008 mcp3008 = new MCP3008(ReferenceVoltage);
+
+
+        private SpeechSynthesizer synthesizer;
+
+
+        public Timer timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            synthesizer = new SpeechSynthesizer();
+
+            mcp3008.Initialize();
+        }
+
+
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs navArgs)
+        {
+            Debug.WriteLine("MainPage::OnNavigatedTo");
+
+            // We will check for light level changes once per second (1000 milliseconds)
+            timer = new Timer(timerCallback, this, 0, 1000);
+        }
+
+        private async void timerCallback(object state)
+        {
+            Debug.WriteLine("\nMainPage::timerCallback");
+            if (mcp3008 == null)
+            {
+                Debug.WriteLine("MainPage::timerCallback not ready");
+                return;
+            }
+            int lowPotReadVal;
+            int highPotReadVal;
+            int cdsReadVal;
+            float lowPotVoltage;
+            float highPotVoltage;
+            float cdsVoltage;
+
+            eState newState = eState.JustRight;
+
+            lowPotReadVal = await mcp3008.ReadADC(LowPotentiometerADCChannel);
+            highPotReadVal = await mcp3008.ReadADC(HighPotentiometerADCChannel);
+            cdsReadVal = await mcp3008.ReadADC(CDSADCChannel);
+
+            lowPotVoltage = mcp3008.ADCToVoltage(lowPotReadVal);
+            highPotVoltage = mcp3008.ADCToVoltage(highPotReadVal);
+            cdsVoltage = mcp3008.ADCToVoltage(cdsReadVal);
+
+            Debug.WriteLine(String.Format("Read values {0}, {1}, {2} ", lowPotReadVal, highPotReadVal, cdsReadVal));
+            Debug.WriteLine(String.Format("Voltages {0}, {1}, {2} ", lowPotVoltage, highPotVoltage, cdsVoltage));
+
+            if (cdsVoltage < lowPotVoltage)
+            {
+                newState = eState.TooDark;
+            }
+
+            if (cdsVoltage > highPotVoltage)
+            {
+                newState = eState.TooBright;
+            }
+
+            await CheckForStateChange(newState);
+        }
+
+        private async Task CheckForStateChange(eState newState)
+        {
+            if (newState != CurrentState)
+            {
+                String whatToSay;
+
+                switch (newState)
+                {
+                    case eState.JustRight:
+                        {
+                            whatToSay = JustRightLightString;
+                        }
+                        break;
+
+                    case eState.TooBright:
+                        {
+                            whatToSay = HighLightString;
+                        }
+                        break;
+
+                    case eState.TooDark:
+                        {
+                            whatToSay = LowLightString;
+                        }
+                        break;
+
+                    default:
+                        {
+                            whatToSay = "unexpected value";
+                        }
+                        break;
+
+                }
+
+                await TextToSpeech(whatToSay);
+                CurrentState = newState;
+            }
+        }
+
+        private async Task TextToSpeech(String textToSpeak)
+        {
+            Debug.WriteLine(String.Format("MainPage::TextToSpeech {0}", textToSpeak));
+            // Because we are running somewhere other than the UI thread and we need to talk to a UI element (the media control)
+            // we need to use the dispatcher to move the calls to the right thread.
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High,
+            async () =>
+            {
+                SpeechSynthesisStream synthesisStream;
+                try
+                {
+                    //creating a stream from the text which can be played using media element. This new API converts text input into a stream.
+                    synthesisStream = await synthesizer.SynthesizeTextToStreamAsync(textToSpeak);
+                }
+                catch (Exception)
+                {
+                    synthesisStream = null;
+                }
+                // if the SSML stream is not in the correct format throw an error message to the user
+                if (synthesisStream == null)
+                {
+                    Debug.WriteLine("unable to synthesize text");
+                    return;
+                }
+
+                // start this audio stream playing
+                media.AutoPlay = true;
+                media.SetSource(synthesisStream, synthesisStream.ContentType);
+                media.Play();
+            }
+            );
+
+        }
+    }
+}
