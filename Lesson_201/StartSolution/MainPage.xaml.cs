407dc2e22fd9d779eba09b789f61e75c61278038
David Shoemaker <David.Shoemaker@microsoft.com>
2015-09-18 10:52:53 -0700

    Lesson 201 starting solution

---
+++ Lesson_201/StartSolution/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Lesson_201
+{
+    /// <summary>
+    /// The application main page.  Because we are running headless we will not see anything
+    /// even though it is begin generated at runtime.  This acts as the main entry point for the 
+    /// application functionality.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // This method will be called by the application framework when the page is first loaded.
+        protected override async void OnNavigatedTo(NavigationEventArgs navArgs)
+        {
+            Debug.WriteLine("MainPage::OnNavigatedTo");
+
+        }
+    }
