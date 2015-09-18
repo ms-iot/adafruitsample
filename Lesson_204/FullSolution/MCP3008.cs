45f6d1f5015c46c2de3baec021b75d5b3ebb0d02
David Shoemaker <David.Shoemaker@microsoft.com>
2015-09-18 10:54:29 -0700

    Updates to lesson.

--- Lesson_204/FullSolution/MCP3008.cs
+++ Lesson_204/FullSolution/MCP3008.cs
@@ -11,39 +11,49 @@ namespace Lesson_204
 {
     class MCP3008
     {
+        // Constants for the SPI controller chip interface
         private SpiDevice mcp3008;
         const int SPI_CHIP_SELECT_LINE = 0;  // SPI0 CS0 pin 24
+
+        // ADC chip operation constants
         const byte MCP3008_SingleEnded = 0x08;
         const byte MCP3008_Differential = 0x00;
 
+        // These are used when we calculate the voltage from the ADC units
         float ReferenceVolgate;
-
         public const uint Min = 0;
         public const uint Max = 1023;
 
+
         public MCP3008(float referenceVolgate)
         {
+            Debug.WriteLine("MCP3008::New MCP3008");
+
+            // Store the reference voltage value for later use in the voltage calculation.
             ReferenceVolgate = referenceVolgate;
         }
 
+        /// <summary>
+        /// This method is used to configure the Pi2 to communicate over the SPI bus to the MCP3008 ADC chip.
+        /// </summary>
         public async void Initialize()
         {
+            Debug.WriteLine("MCP3008::Initialize");
             try
             {
+                // Setup the SPI bus configuration
                 var settings = new SpiConnectionSettings(SPI_CHIP_SELECT_LINE);
                 settings.ClockFrequency = 3600000;                              // 3.6MHz is the rated speed of the MCP3008 at 5v
                 settings.Mode = SpiMode.Mode0;
 
-
-
+                // Ask Windows for the list of SpiDevices
                 string aqs = SpiDevice.GetDeviceSelector();                     /* Get a selector string that will return all SPI controllers on the system */
                 var dis = await DeviceInformation.FindAllAsync(aqs);            /* Find the SPI bus controller devices with our selector string             */
-                mcp3008 = await SpiDevice.FromIdAsync(dis[0].Id, settings);    /* Create an SpiDevice with our bus controller and SPI settings             */
+                mcp3008 = await SpiDevice.FromIdAsync(dis[0].Id, settings);     /* Create an SpiDevice with our bus controller and SPI settings             */
                 if (mcp3008 == null)
                 {
                     Debug.WriteLine(
-                        "SPI Controller {0} is currently in use by " +
-                        "another application. Please ensure that no other applications are using SPI.",
+                        "SPI Controller {0} is currently in use by another application. Please ensure that no other applications are using SPI.",
                         dis[0].Id);
                     return;
                 }
@@ -56,34 +66,22 @@ namespace Lesson_204
             }
         }
 
-
-        bool Init = false;
-        private async Task begin()
+        /// <summary> 
+        /// This method does the actual work of communicating over the SPI bus with the chip.
+        /// To line everything up for ease of reading back (on byte boundary) we 
+        /// will pad the command start bit with 7 leading "0" bits
+        ///
+        /// Write 0000 000S GDDD xxxx xxxx xxxx
+        /// Read  ???? ???? ???? ?N98 7654 3210
+        /// S = start bit
+        /// G = Single / Differential
+        /// D = Chanel data 
+        /// ? = undefined, ignore
+        /// N = 0 "Null bit"
+        /// 9-0 = 10 data bits
+        /// </summary>
+        public int ReadADC(byte whichChannel)
         {
-            Debug.WriteLine("MCP3008::begin");
-
-            // No startup requirements for this part.
-            await Task.Delay(5);
-
-            Init = true;
-        }
-
-        public async Task<int> ReadADC(byte whichChannel)
-        {
-            // To line everything up for ease of reading back (on byte boundary) we 
-            // will pad the command start bit with 7 leading "0" bits
-
-            // Write 0000 000S GDDD xxxx xxxx xxxx
-            // Read  ???? ???? ???? ?N98 7654 3210
-            // S = start bit
-            // G = Single / Differential
-            // D = Chanel data 
-            // ? = undefined, ignore
-            // N = 0 "Null bit"
-            // 9-0 = 10 data bits
-
-            if (!Init) await begin();
-
             byte command = whichChannel;
             command |= MCP3008_SingleEnded;
             command <<= 4;
@@ -97,8 +95,6 @@ namespace Lesson_204
             int sample = readBuf[2] + ((readBuf[1] & 0x03) << 8);
             int s2 = sample & 0x3FF;
             Debug.Assert(sample == s2);
-
-            //Debug.WriteLine("MCP3008::ReadADC C:{0} {1}", whichChannel, sample.ToString());
 
             return sample;
         }
--- Lesson_204/FullSolution/MainPage.xaml.cs
+++ Lesson_204/FullSolution/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using System.Threading.Tasks;
@@ -26,40 +27,49 @@ namespace Lesson_204
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        const float ReferenceVoltage = 3.3F;
+        // Use for configuration of the MCP3008 class voltage formula
+        const float ReferenceVoltage = 5.0F;
+
+        // Values for which channels we will be using from the ADC chip
         const byte LowPotentiometerADCChannel = 0;
         const byte HighPotentiometerADCChannel = 1;
         const byte CDSADCChannel = 2;
+
+        // Some strings to let us know the current state.
         const string JustRightLightString = "Ah, just right";
         const string LowLightString = "I need a light";
         const string HighLightString = "I need to wear shades";
 
+        // Some internal state information
         enum eState { unknown, JustRight, TooBright, TooDark};
         eState CurrentState = eState.unknown;
 
+        // Our ADC Chip class
         MCP3008 mcp3008 = new MCP3008(ReferenceVoltage);
 
-
+        // The Windows Speech API interface
         private SpeechSynthesizer synthesizer;
 
-
+        // A timer to control how often we check the ADC values.
         public Timer timer;
 
         public MainPage()
         {
             this.InitializeComponent();
 
+            // Create a new SpeechSynthesizer instance for later use.
             synthesizer = new SpeechSynthesizer();
 
+            // Initialize the ADC chip for use
             mcp3008.Initialize();
         }
-
-
-
 
         protected override void OnNavigatedTo(NavigationEventArgs navArgs)
         {
             Debug.WriteLine("MainPage::OnNavigatedTo");
+
+            // This will get our pin on the world map showing everyone we are running the sample.
+            MakeWebAPICall();
 
             // We will check for light level changes once per second (1000 milliseconds)
             timer = new Timer(timerCallback, this, 0, 1000);
@@ -73,41 +83,43 @@ namespace Lesson_204
                 Debug.WriteLine("MainPage::timerCallback not ready");
                 return;
             }
-            int lowPotReadVal;
-            int highPotReadVal;
-            int cdsReadVal;
-            float lowPotVoltage;
-            float highPotVoltage;
-            float cdsVoltage;
 
+            // The new light state, assume it's just right to start.
             eState newState = eState.JustRight;
 
-            lowPotReadVal = await mcp3008.ReadADC(LowPotentiometerADCChannel);
-            highPotReadVal = await mcp3008.ReadADC(HighPotentiometerADCChannel);
-            cdsReadVal = await mcp3008.ReadADC(CDSADCChannel);
+            // Read from the ADC chip the current values of the two pots and the photo cell.
+            int lowPotReadVal = mcp3008.ReadADC(LowPotentiometerADCChannel);
+            int highPotReadVal = mcp3008.ReadADC(HighPotentiometerADCChannel);
+            int cdsReadVal = mcp3008.ReadADC(CDSADCChannel);
 
-            lowPotVoltage = mcp3008.ADCToVoltage(lowPotReadVal);
-            highPotVoltage = mcp3008.ADCToVoltage(highPotReadVal);
-            cdsVoltage = mcp3008.ADCToVoltage(cdsReadVal);
+            // convert the ADC readings to voltages to make them more friendly.
+            float lowPotVoltage = mcp3008.ADCToVoltage(lowPotReadVal);
+            float highPotVoltage = mcp3008.ADCToVoltage(highPotReadVal);
+            float cdsVoltage = mcp3008.ADCToVoltage(cdsReadVal);
 
+            // Let us know what was read in.
             Debug.WriteLine(String.Format("Read values {0}, {1}, {2} ", lowPotReadVal, highPotReadVal, cdsReadVal));
             Debug.WriteLine(String.Format("Voltages {0}, {1}, {2} ", lowPotVoltage, highPotVoltage, cdsVoltage));
 
+            // Compute the new state by first checking if the light level is too low
             if (cdsVoltage < lowPotVoltage)
             {
                 newState = eState.TooDark;
             }
 
+            // And now check if it too high.
             if (cdsVoltage > highPotVoltage)
             {
                 newState = eState.TooBright;
             }
 
+            // Use another method to determine what to do with the state.
             await CheckForStateChange(newState);
         }
 
         private async Task CheckForStateChange(eState newState)
         {
+            // Checks for state changes and does something when one is detected.
             if (newState != CurrentState)
             {
                 String whatToSay;
@@ -137,10 +149,12 @@ namespace Lesson_204
                             whatToSay = "unexpected value";
                         }
                         break;
-
                 }
 
+                // Use another method to wrap the speech synthesis functionality.
                 await TextToSpeech(whatToSay);
+
+                // Update the current state for next time.
                 CurrentState = newState;
             }
         }
@@ -150,33 +164,30 @@ namespace Lesson_204
             Debug.WriteLine(String.Format("MainPage::TextToSpeech {0}", textToSpeak));
             // Because we are running somewhere other than the UI thread and we need to talk to a UI element (the media control)
             // we need to use the dispatcher to move the calls to the right thread.
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High,
-            async () =>
-            {
-                SpeechSynthesisStream synthesisStream;
-                try
+            await Dispatcher.RunAsync(
+                Windows.UI.Core.CoreDispatcherPriority.High,
+                async () =>
                 {
-                    //creating a stream from the text which can be played using media element. This new API converts text input into a stream.
+                    SpeechSynthesisStream synthesisStream;
+
+                    //creating a stream from the text which can be played using media element. This API converts text input into a stream.
                     synthesisStream = await synthesizer.SynthesizeTextToStreamAsync(textToSpeak);
-                }
-                catch (Exception)
-                {
-                    synthesisStream = null;
-                }
-                // if the SSML stream is not in the correct format throw an error message to the user
-                if (synthesisStream == null)
-                {
-                    Debug.WriteLine("unable to synthesize text");
-                    return;
-                }
 
-                // start this audio stream playing
-                media.AutoPlay = true;
-                media.SetSource(synthesisStream, synthesisStream.ContentType);
-                media.Play();
-            }
+                    // start this audio stream playing
+                    media.AutoPlay = true;
+                    media.SetSource(synthesisStream, synthesisStream.ContentType);
+                    media.Play();
+                }
             );
-
         }
+
+        // This will put our pin on the world map of makers
+        // Go to ENTER FINAL LINK HERE to view your pin
+        public void MakeWebAPICall()
+        {
+            HttpClient client = new HttpClient();
+            client.GetStringAsync("http://adafruitsample.azurewebsites.net/api?Lesson=204");
+        }
+
     }
 }
