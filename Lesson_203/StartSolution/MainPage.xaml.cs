7035dff51349c94d7db2724a93793ec40e203faa
Aparajita Dutta <apdutta@microsoft.com>
2015-09-22 13:33:02 -0700

    Deleting web API calls from lesson 203

--- Lesson_203/StartSolution/MainPage.xaml.cs
+++ Lesson_203/StartSolution/MainPage.xaml.cs
@@ -17,9 +17,6 @@ namespace Lesson_203
     {
         //A class which wraps the barometric sensor
         
-        //A web client for making web API calls
-        
-
         public MainPage()
         {
             this.InitializeComponent();
@@ -34,31 +31,18 @@ namespace Lesson_203
                 //Create a new object for our barometric sensor class
                 
                 //Initialize the sensor
-                
-
-                //Create a new web client                
+                              
 
                 //Create variables to store the sensor data: temperature, pressure and altitude. 
                 //Initialize them to 0.
                
 
                 //Create a constant for pressure at sea level. 
-                //This is based on your local sea level pressure (Unit: Hectopascal)
-                
+                //This is based on your local sea level pressure (Unit: Hectopascal)              
 
                 //Read 10 samples of the data
                 
-
-                //Write the average values to your debug console
-                
-
-                //Create the grouped content to send to the web API
-                
-
-                //Create a response message to post the data
-                
-                //Ensure the data was posted successfully
-                
+                //Write the values to your debug console                            
 
             }
             catch (Exception ex)
